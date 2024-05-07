cdfcba515cced474c64e2686746f5a115a301fda
Isaac71412079 <isaac.rivero@ucb.edu.co>
2024-05-07 10:06:38 -0400

    no encuentra proyectos si se tiene una lista vacia

--- src/buscador.spec.js
+++ src/buscador.spec.js
@@ -1,5 +1,5 @@
 describe("Buscador", () =>{
-    it("doesn´t finds any project match when its an empty array",() => {
+    it("doesn´t finds any project match when having an empty array",() => {
         let proyectos = [];
         expect(buscarProyecto("exercise1",proyectos)).toEqual("");
         
